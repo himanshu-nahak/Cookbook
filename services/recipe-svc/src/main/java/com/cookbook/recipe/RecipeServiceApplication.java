ae6d1c2cc789ed77ea28a42a2a7beeccd01f0d2d
Himanshu <himanshunahak13@gmail.com>
2025-05-31 12:21:39 +0530

    Initial commit - Recipe Svc

--- services/recipe-svc/src/main/java/com/cookbook/recipe/RecipeServiceApplication.java
+++ services/recipe-svc/src/main/java/com/cookbook/recipe/RecipeServiceApplication.java
@@ -1,4 +1,4 @@
-package org.nahak;
+package com.cookbook.recipe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- services/recipe-svc/src/test/java/com/cookbook/RecipeServiceApplicationTests.java
+++ services/recipe-svc/src/test/java/com/cookbook/RecipeServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package org.nahak;
+package com.cookbook;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
